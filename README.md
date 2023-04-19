723c418c0bc735257c73861c391983f4f535a6e9
Frikk Jarl <32601161+FRIKKern@users.noreply.github.com>
2023-04-20 01:15:03 +0200

    Making it better!

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # SnipeScript
-A custom TypeScript compiler that compiles a specific folder and generates an error tree.
+
+SnipeScript is a custom TypeScript compiler that compiles a specific folder and generates an error tree. It allows you to selectively filter errors by error codes and folder paths. The output of the program is a JSON file containing the error tree, making it easy to analyze and manage the TypeScript compilation errors.
+
+## Features
+
+- Compiles a specific folder containing TypeScript files.
+- Generates an error tree for easier error analysis.
+- Allows filtering errors by error codes and folder paths.
+- Saves the output as a JSON file.
+
+## Installation
+
+To use SnipeScript, you will need to have Node.js installed on your system. After that, you can install SnipeScript as a global package by running the following command:
+
+```bash
+    npm install -g snipescript
+```
+
+## Usage
+
+To compile a specific folder and generate the error tree, run the following command:
+
+```bash
+
+    snipescript <folder-path>
+
+```
+
+Replace <folder-path> with the path to the folder containing your TypeScript files.
+
+The output will be saved to an output.json file in the current directory.
+
+## Configuration
+
+You can customize the behavior of SnipeScript by modifying the script. For example, you can add or remove error codes to ignore by updating the ignoredErrorCodes array:
+
+```javascript
+const ignoredErrorCodes = [17004, 1259, 1208, 2339, 2351]; // Add the error codes you want to ignore
+```
+
+You can also update the TypeScript compiler options by modifying the options object:
+
+```javascript
+const options = {
+  target: ts.ScriptTarget.ES2020,
+  module: ts.ModuleKind.CommonJS,
+  strict: true,
+  noEmit: true,
+};
+```
+
+## License
+
+SnipeScript is released under the MIT License.
--- snipescript.js
+++ snipescript.js
@@ -10,7 +10,9 @@ function readFilesRecursively(folderPath) {
   for (const entry of entries) {
     const entryPath = path.join(folderPath, entry.name);
     if (entry.isDirectory()) {
-      files.push(...readFilesRecursively(entryPath));
+      if (entry.name !== "node_modules") {
+        files.push(...readFilesRecursively(entryPath));
+      }
     } else if (entry.isFile()) {
       files.push(entryPath);
     }
@@ -19,11 +21,43 @@ function readFilesRecursively(folderPath) {
   return files;
 }
 
+function filterErrorsByFolderPath(errorTree, folderPath) {
+  const filteredErrors = {};
+
+  for (const [filePath, errors] of Object.entries(errorTree)) {
+    if (filePath.startsWith(folderPath)) {
+      filteredErrors[filePath] = errors;
+    }
+  }
+
+  return filteredErrors;
+}
+
+function createFileNode(filePath, errors) {
+  const errorFree = !errors || errors.length === 0;
+  return {
+    name: path.basename(filePath),
+    type: "file",
+    errorFree,
+    errors,
+  };
+}
+
+function createDirectoryNode(directoryPath, children) {
+  return {
+    name: path.basename(directoryPath),
+    type: "directory",
+    children,
+  };
+}
+
 function compile(fileNames, options) {
   const program = ts.createProgram(fileNames, options);
   const allDiagnostics = ts.getPreEmitDiagnostics(program);
 
   const errors = {};
+  const ignoredErrorCodes = [17004, 1259, 1208, 2339, 2351]; // Add the error codes you want to ignore
+
   allDiagnostics.forEach((diagnostic) => {
     if (diagnostic.file) {
       const fileName = diagnostic.file.fileName;
@@ -36,43 +70,70 @@ function compile(fileNames, options) {
         "\n"
       );
 
-      if (!errors[fileName]) {
-        errors[fileName] = [];
-      }
+      // Ignore specific errors by their error code
+      if (!ignoredErrorCodes.includes(diagnostic.code)) {
+        if (!errors[fileName]) {
+          errors[fileName] = [];
+        }
 
-      errors[fileName].push({
-        code: diagnostic.code,
-        message,
-        file: fileName,
-        location: { line, character },
-      });
+        errors[fileName].push({
+          code: diagnostic.code,
+          message,
+          file: fileName,
+          location: { line, character },
+        });
+      }
     }
   });
 
-  return { errors };
+  return errors;
+}
+
+function buildFileTree(folderPath, errors) {
+  const entries = fs.readdirSync(folderPath, { withFileTypes: true });
+  const children = [];
+
+  for (const entry of entries) {
+    const entryPath = path.join(folderPath, entry.name);
+    if (entry.isDirectory()) {
+      children.push(buildFileTree(entryPath, errors));
+    } else if (entry.isFile()) {
+      children.push(createFileNode(entryPath, errors[entryPath]));
+    }
+  }
+
+  return createDirectoryNode(folderPath, children);
 }
 
 function main() {
-  const fileNames = readFilesRecursively(".");
+  const targetFolder = process.argv[2];
+  const fileNames = readFilesRecursively(targetFolder);
   const options = {
     target: ts.ScriptTarget.ES2020,
     module: ts.ModuleKind.CommonJS,
     strict: true,
-    noEmit: true, // Add this line to prevent file generation
+    noEmit: true,
   };
 
-  const targetFolder = process.argv[2];
   const filteredFileNames = fileNames.filter((fileName) =>
     fileName.startsWith(targetFolder)
   );
   const program = ts.createProgram(filteredFileNames, options);
 
-  const result = compile(filteredFileNames, options);
-  if (result.errors && Object.keys(result.errors).length > 0) {
-    console.error("Compilation failed with errors:", result.errors);
-  } else {
-    console.log("Compilation succeeded without errors");
-  }
+  const errors = compile(filteredFileNames, options);
+  const filteredErrors = filterErrorsByFolderPath(errors, targetFolder); // Filter errors based on the folder path
+  const fileTree = buildFileTree(targetFolder, errors);
+
+  console.log(JSON.stringify({ fileTree, errorTree: filteredErrors }, null, 2)); // Log the filtered error tree
+
+  // Save the output to a file
+  fs.writeFileSync(
+    "output.json",
+    JSON.stringify({ fileTree, errorTree: filteredErrors }, null, 2),
+    "utf-8"
+  );
+
+  console.log("Output saved to output.json");
 }
 
 main();
